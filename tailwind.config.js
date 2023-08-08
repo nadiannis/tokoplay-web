012ea94d76fc0797812020fcf2924c7f177c097c
nadiannis <annisanadianeyla@gmail.com>
2023-08-09 02:47:45 +0700

    chore: create custom max-width, background-image, & color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        'screen-12xl': '1920px',
+      },
+      backgroundImage: {
+        'thumbnail-text':
+          'linear-gradient(rgba(20, 25, 30, 0) 0%, rgba(20, 25, 30, 0.8) 100%)',
+      },
+      colors: {
+        shade: {
+          900: '#0e111a',
+        },
+      },
+    },
   },
   plugins: [],
 };
