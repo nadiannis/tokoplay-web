4ad7d0bbcdc37aef78d30243b7f2a3faef8ef58c
nadiannis <annisanadianeyla@gmail.com>
2023-08-10 23:21:34 +0700

    feat: modify padding & add className props to Container

--- src/components/container/index.jsx
+++ src/components/container/index.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
 
-export default function Container({ children }) {
+export default function Container({ children, className = '' }) {
   return (
-    <div className="w-full max-w-screen-12xl mx-auto px-2.5">{children}</div>
+    <div className={`w-full max-w-screen-12xl mx-auto px-4 ${className}`}>
+      {children}
+    </div>
   );
 }
 
 Container.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 };
