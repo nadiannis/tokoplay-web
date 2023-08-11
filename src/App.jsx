7573947fa1568a73ffc1234b073b9ca06a64e836
nadiannis <annisanadianeyla@gmail.com>
2023-08-11 14:01:49 +0700

    feat: change id to videoId on the route path

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
-          <Route path="/videos/:id" element={<VideoDetailPage />} />
+          <Route path="/videos/:videoId" element={<VideoDetailPage />} />
         </Route>
 
         <Route path="*" element={<NotFoundPage />} />
