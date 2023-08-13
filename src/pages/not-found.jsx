2f3c321158657d6185c2ae7594acef976d295072
nadiannis <annisanadianeyla@gmail.com>
2023-08-14 01:01:31 +0700

    feat: create not found page & modify thumbnail height

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,3 +1,19 @@
+import { Link } from 'react-router-dom';
+
 export default function NotFoundPage() {
-  return <>Not Found Page</>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="bg-gray-900 text-center w-11/12 max-w-md px-4 py-16 rounded-lg">
+        <h1 className="font-bold text-3xl md:text-4xl">404 Not Found</h1>
+        <div className="mt-10">
+          <Link
+            to="/"
+            className="block mt-8 text-sm text-gray-300 underline hover:no-underline active:no-underline"
+          >
+            Back to Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
